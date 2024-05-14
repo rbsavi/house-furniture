db50a5bf8430cdbaca01cff218ad45a4514970e2
rbsavi <164175444+rbsavi@users.noreply.github.com>
2024-05-14 21:27:01 +0000

    house and room page working

--- app/models/room.rb
+++ app/models/room.rb
@@ -10,9 +10,5 @@
 #  user_id    :integer
 #
 class Room < ApplicationRecord
-
-  has_many  :furnitures, class_name: "Furniture", foreign_key: "room_id", dependent: :destroy
-
-  belongs_to :house, required: true, class_name: "House", foreign_key: "house_id", counter_cache: true
   
 end
